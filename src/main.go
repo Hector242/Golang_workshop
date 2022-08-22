5de6304f4f5d9cd8d2e3b1ad5ba2f401d6d3bd2d
hsanchez <hsanchez@edrans.com>
2022-08-22 11:23:56 -0300

    functions

--- src/main.go
+++ src/main.go
@@ -2,31 +2,30 @@ package main
 
 import "fmt"
 
-func main() {
-	//Declaracion de constantes
-	const Hellomessage string = "Hello"
-	const Worldmessage string = "World"
-
-	//Println
-	fmt.Println(Hellomessage, Worldmessage)
-	fmt.Println(Hellomessage, Worldmessage)
-
-	//Printf
-	//Sirve para introducir variables o constantes en el mensaje a imprimir en pantalla
-	const name string = "Hector"
-	const lastname_count int = 2
-
-	fmt.Printf("%s have more than %d lastnames\n", name, lastname_count)
-	fmt.Printf("%v have more than %v lastnames\n", name, lastname_count) // %v la usamos cuando no sepamos el tipo de dato
-
-	// Sprintf
-	//Sprintf sirve para guardar un print y que no se muestre por consola
-	var message string = fmt.Sprintf("%s have more than %d lastnames", name, lastname_count)
+func holamundoFuntion(message string) {
 	fmt.Println(message)
+}
 
-	//data type
-	//El %T nos dice el tipo de dato
-	fmt.Printf("hellomessage: %T\n", Hellomessage)
-	fmt.Printf("lastname count: %T\n", lastname_count)
+func argument(a, b int, c string) {
+	fmt.Println(a, b, c)
+}
 
+func returnOneValue(a int) int {
+	return a * 2
+}
+
+func returnTwoValues(a int) (c, d int) {
+	return a, a * 2
+}
+
+func main() {
+	holamundoFuntion("Hola mundo")
+	argument(1, 2, "Hello")
+
+	var value int = returnOneValue(2)
+	fmt.Println("Value: ", value)
+
+	var value1, value2 int = returnTwoValues(3)
+	fmt.Printf("Value 1: %d", value1)
+	fmt.Printf(" Value 2: %d", value2)
 }
