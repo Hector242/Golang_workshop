db5487d90ff9d56d51e79de26da05d567af3ea27
hsanchez <hsanchez@edrans.com>
2022-08-24 09:47:49 -0300

    conditional and text converter

--- src/main.go
+++ src/main.go
@@ -1,17 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"strconv"
+)
 
 func main() {
-	//for condicional
-	for i := 0; i < 10; i++ {
-		fmt.Println("i: ", i)
+	var value1 int = 1
+	var value2 int = 2
+
+	if value1 == 1 {
+		fmt.Println("El valor es ", value1)
+	} else {
+		fmt.Println("El valor no es 1")
 	}
 
-	//for while
-	j := 0
-	for j < 10 {
-		fmt.Println("j: ", j)
-		j++
+	//With AND
+	if value1 == 1 && value2 == 2 {
+		fmt.Println("El valor es 1 y 2")
+	} else {
+		fmt.Println("El valor no es el correcto")
 	}
+
+	//With OR
+	if value1 == 0 || value2 == 2 {
+		fmt.Println("El valor es 1 o 2")
+	} else {
+		fmt.Println("El valor no es el correcto")
+	}
+
+	//convert text to number
+	//we this method we will get 2 values: value, err.
+	value, err := strconv.Atoi("53") //On value we save the value and in err we will save the error
+	if err != nil {                  //if error does not existed, it will be nil
+		log.Fatal(err) //will print the error
+	}
+	fmt.Println("Value: ", value)
 }
