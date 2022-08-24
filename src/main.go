d165e44c5e5fa3048989e1d81f6850b32892d624
hsanchez <hsanchez@edrans.com>
2022-08-24 10:17:43 -0300

    switch conditional

--- src/main.go
+++ src/main.go
@@ -2,39 +2,25 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"strconv"
 )
 
 func main() {
-	var value1 int = 1
-	var value2 int = 2
 
-	if value1 == 1 {
-		fmt.Println("El valor es ", value1)
-	} else {
-		fmt.Println("El valor no es 1")
+	switch module := 5 % 2; module {
+	case 0:
+		fmt.Println("Es par")
+	default:
+		fmt.Println("Es impar")
 	}
 
-	//With AND
-	if value1 == 1 && value2 == 2 {
-		fmt.Println("El valor es 1 y 2")
-	} else {
-		fmt.Println("El valor no es el correcto")
+	//without condition
+	value := 200
+	switch {
+	case value > 100:
+		fmt.Println("es mayor a 100")
+	case value == 100:
+		fmt.Println("es igual a 100")
+	default:
+		fmt.Println("Es menor a 100")
 	}
-
-	//With OR
-	if value1 == 0 || value2 == 2 {
-		fmt.Println("El valor es 1 o 2")
-	} else {
-		fmt.Println("El valor no es el correcto")
-	}
-
-	//convert text to number
-	//we this method we will get 2 values: value, err.
-	value, err := strconv.Atoi("53") //On value we save the value and in err we will save the error
-	if err != nil {                  //if error does not existed, it will be nil
-		log.Fatal(err) //will print the error
-	}
-	fmt.Println("Value: ", value)
 }
