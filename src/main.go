404e2da404850a34cd81227c7e3c07f325ec1a8a
c-hsanchez <c_hsanchez@groupon.com>
2022-09-06 17:16:20 -0300

    structs with pointers

--- src/main.go
+++ src/main.go
@@ -1,15 +1,47 @@
 package main
 
 import (
-	"curso_golang_platzi/src/mypackage"
 	"fmt"
 )
 
-func main() {
-	var mycar mypackage.CarPublic
-	mycar.Brand = "ferrary"
-	mycar.Year = 2022
-	fmt.Println(mycar)
+type pc struct {
+	ram   int
+	disk  int
+	brand string
+}
 
-	mypackage.PrintMessagePublic()
+func (myPc pc) ping() {
+	fmt.Println("la marca es: ", myPc.brand)
+}
+
+func (myPc *pc) duplicateRam() {
+	myPc.ram = myPc.ram * 2
+}
+
+func main() {
+
+	a := 50
+	b := &a
+
+	fmt.Println("La direccion de memoria es: ", b)
+	fmt.Println("El valor en esa direccion es: ", *b)
+
+	*b = 100
+	fmt.Println("Nuevo valor de la variable a es: ", a)
+
+	// ejemplo de structs con funciones
+	var myPc pc
+	myPc.brand = "asus"
+	myPc.disk = 100
+	myPc.ram = 32
+
+	fmt.Println("My pc tiene: ", myPc)
+
+	myPc.ping()
+
+	// usando punteros con los struct en funciones
+	fmt.Println("Estado actual de mi pc: ", myPc)
+
+	myPc.duplicateRam()
+	fmt.Println("Ram duplicada: ", myPc)
 }
